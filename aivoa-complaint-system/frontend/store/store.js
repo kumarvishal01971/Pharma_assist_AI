d68f9778431697bb715488a5d9e3a74b752ea377
Vishal Kumar <kumarvishal01971@gmail.com>
2026-08-02 18:28:05 +0530

    Delete aivoa-complaint-system/frontend/store directory

--- aivoa-complaint-system/frontend/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import complaintReducer from './complaintSlice'
-
-export const store = configureStore({
-  reducer: {
-    complaint: complaintReducer,
-  },
-})
